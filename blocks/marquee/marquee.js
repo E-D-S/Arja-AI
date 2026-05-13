f2bf447ca703d20af879c28c0e5ffde4cbe937ae
e-d-s <edstestoperations@gmail.com>
2026-05-13 15:26:45 +0530

    Update marquee.js

--- blocks/marquee/marquee.js
+++ blocks/marquee/marquee.js
@@ -1,5 +1,4 @@
 export default function decorate(block) {
   const singleLine = [...block.innerText];
   block.classList.add(`marquee-${singleLine.length}-arjaAI`);
-  block.replaceWith(singleLine);
 }
