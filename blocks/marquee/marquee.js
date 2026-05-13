ec3fa849d2d1b74f1de0c07aa5e618e0f2171524
e-d-s <edstestoperations@gmail.com>
2026-05-13 13:52:09 +0530

    Update marquee.js

--- blocks/marquee/marquee.js
+++ blocks/marquee/marquee.js
@@ -1,1 +1,9 @@
+import { createOptimizedPicture } from '../../scripts/aem.js';
 
+export default function decorate(block) {
+  const singleLine = document.createElement('p');
+forEach((div) => {
+      if (div.children.length === 1 && div.querySelector('p')) div.className = 'marquee';
+    });
+  block.replaceChildren(singleLine);
+}
